66f9fe9fd4c3fc508db81bd51e9279eb84dd5527
Charizard17 <3723esad@gmail.com>
2024-06-18 14:00:22 +0200

    add-task and reminder functionalities implemented

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -3,16 +3,17 @@ import { NgFor } from '@angular/common';
 import { TaskService } from '../../services/task.service';
 import { Task } from '../../Task';
 import { TaskItemComponent } from '../task-item/task-item.component';
+import { AddTaskComponent } from '../add-task/add-task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [NgFor, TaskItemComponent],
+  imports: [NgFor, TaskItemComponent, AddTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent implements OnInit {
-  private tasks: Task[] = [];
+  tasks: Task[] = [];
 
   constructor(private taskService: TaskService) {}
 
@@ -27,5 +28,14 @@ export class TasksComponent implements OnInit {
         console.error('Error deleting task:', error);
       }
     );
+  }
+
+  toggleReminder(task: Task) {
+    task.reminder = !task.reminder;
+    this.taskService.updateTaskReminder(task).subscribe();
+  }
+
+  addTask(task: Task) {
+    this.taskService.addTask(task).subscribe((task) => this.tasks.push(task));
   }
 }
